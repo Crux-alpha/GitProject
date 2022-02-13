bbc92636e6263bcd1cd93027aef2e50e37563dfd
Cruα <1992980352@qq.com>
2022-02-13 19:05:09 +0800

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+	
+	public static void main(String[] args){
+		String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
+		System,out.println("save time to " + time);
+
+	}
+}
