52c8b660e0839fbcd4accb130a7b801a3e167908
Crux-alpha <99722252+Crux-alpha@users.noreply.github.com>
2022-02-15 17:37:05 +0800

    111

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,6 @@ public class HelloWorld{
 	public static void main(String[] args){
 		String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
 		System,out.println("save time to " + time);
-		System.out.println("hot-fix");
-
-
+		System.out.println("broswer edition");
 	}
 }
